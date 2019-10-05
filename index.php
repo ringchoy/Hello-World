016409588a205eb6e780c37061621a415a55f573
ringchoy <41923302+ringchoy@users.noreply.github.com>
2019-10-05 14:01:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<?php $connect = mysql_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7");
+<?php ?>
+<!DOCTYPE html>
+<html>
+    <head><!--header stuff--></head>
+    <body>
+      <?php $connect = mysql_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7");
                 if (!$connect) {
                     die(mysql_error());
                 }
                 mysql_select_db("heroku_f4436271c441c5d");
                 $results = mysql_query("SELECT * FROM testhtml");
-?>
-<html>
-    <head><!--header stuff--></head>
-    <body>
+      ?>
         <table>
             <?php while($row = mysql_fetch_array($results)): ?>
                 <tr>
