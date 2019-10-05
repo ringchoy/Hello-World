cf4e5d1ae39f53dd108138d694907c013e9b27c3
ringchoy <41923302+ringchoy@users.noreply.github.com>
2019-10-05 14:53:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <html>
     <head><!--header stuff--></head>
     <body>
-      <?php $connect = mysqli_connect("lus-cdbr-iron-east-05.cleardb.net:3306","ba34f3f8d9d386", "6206b3d7", "heroku_f4436271c441c5d");
-                if (!$connect) {
-                    $results = "flop";
-                    die(mysql_error());
-                }
-         $sql = 'SELECT name FROM testhtml';
-         $results = mysqli_query($connect, $sql);
-        
-         mysqli_close($conn);
+      <?php 
+//        $connect = mysqli_connect("lus-cdbr-iron-east-05.cleardb.net:3306","ba34f3f8d9d386", "6206b3d7", "heroku_f4436271c441c5d");
+//                if (!$connect) {
+//                    $results = "flop";
+//                    die(mysql_error());
+//                }
+//         $sql = 'SELECT name FROM testhtml';
+//         $results = mysqli_query($connect, $sql);
+            $results = "Kyle is doing great";      
+ //        mysqli_close($conn);
 
 //         if (mysqli_num_rows($result) > 0) {
 //            while($row = mysqli_fetch_assoc($result)) {
