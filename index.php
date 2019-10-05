89a3633271256bec6f9a24bcee55f6f79267baf1
ringchoy <41923302+ringchoy@users.noreply.github.com>
2019-10-05 14:11:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
       <?php $connect = mysql_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7");
                 if (!$connect) {
                     die(mysql_error());
+                    $results = "flop";
                 }
                 mysql_select_db("heroku_f4436271c441c5d");
                 $results = "Kyle";
