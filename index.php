80b7eb25456fe06c772580f1ffb7173d3cf1d769
ringchoy <41923302+ringchoy@users.noreply.github.com>
2019-10-05 14:15:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 <html>
     <head><!--header stuff--></head>
     <body>
-      <?php results = "Start";
-        $connect = mysql_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7");
+      <?php $connect = mysql_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7");
                 if (!$connect) {
-                    die(mysql_error());
                     $results = "flop";
+                    die(mysql_error());
                 }
                 mysql_select_db("heroku_f4436271c441c5d");
                 $results = "Kyle";
