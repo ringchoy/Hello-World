b6c5231b77991e973fd6ffae1930d8bbaf2f01ea
ringchoy <41923302+ringchoy@users.noreply.github.com>
2019-10-05 14:08:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <html>
     <head><!--header stuff--></head>
     <body>
-      <?php 
-                $results = "FROM testhtml";
-      ?>
+      <?php $connect = mysql_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7");
+                if (!$connect) {
+                    die(mysql_error());
+                }
+                mysql_select_db("heroku_f4436271c441c5d");
+                $results = mysql_query("SELECT * FROM testhtml");
+        ?>
         <h1> <?php echo $results; ?> </h1>
     </body>
 </html>
