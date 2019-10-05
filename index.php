4ef211711a97feee42f9fc9136ccec4f6a1d4c09
ringchoy <41923302+ringchoy@users.noreply.github.com>
2019-10-05 13:59:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<?php 
-        results = "SELECT * FROM testhtml"
+<?php $connect = mysql_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7");
+                if (!$connect) {
+                    die(mysql_error());
+                }
+                mysql_select_db("heroku_f4436271c441c5d");
+                $results = mysql_query("SELECT * FROM testhtml");
 ?>
-<!DOCTYPE html>
 <html>
     <head><!--header stuff--></head>
     <body>
-        <?php while($row = $results) : ?>
-        $row
-        <?php endwhile;?>
+        <table>
+            <?php while($row = mysql_fetch_array($results)): ?>
+                <tr>
+                   <td><?php echo $row['Name']; ?></td>
+                   <td><?php echo $row['Score']; ?></td>
+                   <td><?php echo $row['Entry_dt']; ?></td>
+               </tr>
+            <?php endwhile; ?>
+        </table>
     </body>
 </html>
