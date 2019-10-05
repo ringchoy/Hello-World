2bd7be2552c9c8f4095d68ddaba2a1422b753814
ringchoy <41923302+ringchoy@users.noreply.github.com>
2019-10-05 14:39:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-<?php ?>
 <!DOCTYPE html>
 <html>
     <head><!--header stuff--></head>
     <body>
       <?php $connect = mysqli_connect("lus-cdbr-iron-east-05.cleardb.net","ba34f3f8d9d386", "6206b3d7", "heroku_f4436271c441c5d");
-                /*if (!$connect) {
+                if (!$connect) {
                     $results = "flop";
                     die(mysql_error());
-                }*/
-                $results = "Kyle";
+                }
+         $sql = 'SELECT name FROM testhtml';
+         $result = mysqli_query($connect, $sql);
+
+//         if (mysqli_num_rows($result) > 0) {
+//            while($row = mysqli_fetch_assoc($result)) {
+//               echo "Name: " . $row["name"]. "<br>";
+//            }
+//         } else {
+//            echo "0 results";
+//         }
+//                //$results = "Kyle";
         ?>
+        
         <h1> Double Check Before PHP out</h1>
         <h1> <?php echo $results; ?> </h1>
         <h1> Triple Check after PHP </h1>
